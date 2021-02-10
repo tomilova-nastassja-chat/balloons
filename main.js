d933cfcb576fcad44bb217e45fd0fb729a7b651d
tomilova-nastya <nastya.aisha@gmail.com>
2021-02-10 19:31:22 +0300

    динамическии меняющийся поток шариков

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let gameTimeInSeconds = 60;
 
 setTimeout(() => {
     clearTimeout(screenTimerId); // остановка отсчета времени на экране
+    clearTimeout(ballonsFlowTimeoutId); // остановка создания шариков
     document.querySelector("#gameTime").parentNode.innerHTML = "Время вышло";
     document.querySelector("#gameScore").innerHTML = `Лопнуто шариков ${gameScore}<br> Пропущено шариков ${balloonsCount - gameScore}`;
 }, gameTimeInSeconds * 1000);
@@ -37,7 +38,7 @@ class Balloon {
     }
 
     clearBalloon() {
-        ctx.clearRect(this.X - this.Radius, this.Y - this.Radius, this.Radius*2, this.Radius*2);
+        ctx.clearRect(this.X - this.Radius, this.Y - this.Radius + this.Speed, this.Radius * 2, this.Radius * 2);
     }
 
     runBalloon() {
@@ -45,7 +46,7 @@ class Balloon {
             this.clearBalloon();
             this.Y = this.Y - this.Speed;
             this.drawBalloon();
-        }, 100);
+        }, 50);
     }
 }
 
@@ -57,17 +58,30 @@ let createRandomBalloon = function (acceleration) {
     let randomX = getRandomNumber(randomRadius, gameZoneSize - randomRadius); // шарик всегда вмещается в ширину игрового поля
     let startY = gameZoneSize + randomRadius; // шарик появляется за видимой областью игрового поля
     let randomColor = balloonsColors[getRandomNumber(0, balloonsColors.length)];
-    let randomSpeed = getRandomNumber(1, 5);
+    let randomSpeed = getRandomNumber(1, 3);
 
     let balloon = new Balloon(randomX, startY, randomRadius, randomColor, randomSpeed + acceleration);
     balloon.runBalloon();
 }
 
-let getRandomNumber = function(from, to) {
+let getRandomNumber = function (from, to) {
     return Math.floor(Math.random() * (to - from)) + from;
 }
 
 // Создание потока шариков
-createBalloonsFlow = function() {
-    createRandomBalloon(0);
+let ballonsFlowTimeoutId = 0;
+let acceleration = 1;
+
+createRandomBalloon(acceleration);
+createBalloonsFlow = function () {
+    clearTimeout(ballonsFlowTimeoutId);
+
+    ballonsFlowTimeoutId = setInterval(() => {
+        acceleration = acceleration + 0.05;
+        createRandomBalloon(acceleration);
+
+        createBalloonsFlow();
+    }, 3500 - acceleration * 1000); // уменьшение времени таймаута, чтобы шариков становилось больше
 }
+
+createBalloonsFlow();
